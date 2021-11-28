d94b0410d995c5c22c848eb10935f57e5e89a0fe
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2021-11-28 22:50:33 +0200

    Add files via upload
    Сверстано меню сайта, но не адаптировано под мобильные устройства

---
+++ src/Components/Carousel.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Form, FormControl} from 'react-bootstrap';
+
+export default class Carousel extends Component {
+    render() {
+        return(
+                <div>
+	</div>
+        )
+    }
+}
