c641b395a234a559df4bc64ceb4f3c25ecf7bd56
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-02-01 20:58:26 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header.js';
+import Footer from './Components/Footer';
 
 //роутинг
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './Pages/Home.js';
 import Consoles from './Pages/Consoles.js';
 import Games from './Pages/Games.js';
 import Community from './Pages/Community.js';
 
-
 function App() {
   return (
-   
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/consoles" element={<Consoles />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
-      </BrowserRouter>     
-   
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/games" exact element={<Games/>} />
+        <Route path="/consoles" exact element={<Consoles/>} />
+        <Route path="/community" exact element={<Community/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>          
   );
 }
 
