fbb23e5241f4b3eb9f2b65cfbce765ee6a93360c
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-02-02 17:35:44 +0200

    Add files via upload

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 //Импорт изображений
 import Instagram from '../assets/images/icons/Instagram.svg';
@@ -6,27 +6,26 @@ import Facebook from '../assets/images/icons/Faacebook.svg';
 import Twitter from '../assets/images/icons/Twitter.svg';
 
 
-export default class Footer extends Component {
-    render() {
+export default function Footer () {
         return(     
             <>    
             <footer>
-                <div className="footer_main">
-                    <div className="footer_container">
-                        <div className="footer_content">
-                            <div className="footer_leftmenu">
-                                <h4 className="footer_title">XBOX</h4>
-                                <ul className="footer_link">
+                <div className="page-container">
+                    <div className="footer__container">
+                        <div className="footer__content">
+                            <div>
+                                <h4 className="footer__title">XBOX</h4>
+                                <ul className="footer__link">
                                     <li><a href="#">Support</a></li>
                                     <li><a href="#">Contact us</a></li>
-                                    <li><a href="#">Privacy & cookies </a></li>
+                                    <li><a href="#">Privacy &#38; cookies </a></li>
                                     <li><a href="#">Terms of use </a></li>
                                 </ul>
                             </div>
 
-                            <div className="footer_center">
-                                <h4 className="footer_title">Developers</h4>
-                                <ul className="footer_link">
+                            <div>
+                                <h4 className="footer__title">Developers</h4>
+                                <ul className="footer__link">
                                     <li><a href="#">Games</a></li>
                                     <li><a href="#">Windows 10</a></li>
                                     <li><a href="#">Creators Program</a></li>
@@ -34,9 +33,9 @@ export default class Footer extends Component {
                                 </ul>
                             </div>
 
-                            <div className="footer_rightmenu">
-                                <h4 className="footer_title">Follow Us</h4>
-                                <div className="footer_icons">
+                            <div>
+                                <h4 className="footer__title">Follow Us</h4>
+                                <div className="footer__icons">
                                     <img src={Facebook} alt="facebook" />
                                     <img src={Twitter} alt="twitter" />
                                     <img src={Instagram} alt="instagram"/>
@@ -44,15 +43,14 @@ export default class Footer extends Component {
                             </div>
                         </div>
 
-                    <div className="footer_line"></div>
-                    <div className="footer_copy">
-                        <p className="footer_copytext">English (United States)</p>
-                        <p className="footer_copytext"> © Microsoft 2017</p>
+                    <div className="footer__line"></div>
+                    <div className="footer__copy">
+                        <p>English (United States)</p>
+                        <p> © Microsoft 2017</p>
                     </div>
                 </div>
                 </div>
             </footer> 
             </>       
         )
-    }
 }
