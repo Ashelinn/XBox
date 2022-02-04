318c811e5a235b9584d5aeee6426543ffaefdcdb
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-02-04 17:05:48 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
 import React from "react";
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header.js';
+import Footer from './Components/Footer';
 
 //роутинг
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from './Pages/Home.js';
-import Consoles from './Pages/Consoles.js';
-import Games from './Pages/Games.js';
-import Community from './Pages/Community.js';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from './Pages/Home';
+import Consoles from './Pages/Consoles';
+import Games from './Pages/Games';
+import Community from './Pages/Community';
+import Details from './Pages/Details';
+import Acsessorise from './Pages/Acsessorise';
+import data from './bdgames.json';
 
 
 function App() {
   return (
-   
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/consoles" element={<Consoles />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
-      </BrowserRouter>     
-   
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/pcgames" exact element={<Games games = {data.games} title = 'PC Games' />} />
+        <Route path="/consolegames" exact element={<Games games = {data.consolegames} title = 'Console Games' />} />
+        <Route path="/consoles" exact element={<Consoles/>} />
+        <Route path="/community" exact element={<Community/>} />
+        <Route path="/details" exact element={<Details/>} />
+        <Route path="/accessories" exact element={<Acsessorise/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>          
   );
 }
 
