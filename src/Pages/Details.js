c3e0468f3a396be5f38ebc6fcd72404a73d19ad2
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-02-04 17:01:35 +0200

    Add files via upload

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -1,76 +1,81 @@
-import React, {Component} from 'react';
+import React from 'react';
+import ReactPlayer from "react-player"
+
 import War from '../assets/images/War.jpg';
 import War1 from '../assets/images/WarOne.jpg';
 import War2 from '../assets/images/WarTwo.jpg';
-import WarVideo from '../assets/images/video/War.mp4';
+
+
 
 function Details () {
+
     return (
         <div>
-             <div class="bgfon">
-                <div class="mainImage"  styles={{ background:`url(${War})` }} >
-                    <div class="mainImage_content">
-                        <div class="left-65"></div>
+             <div className="bgfon">
+                <div className="mainImage"  style={{ backgroundImage:`url(${War})`, backgroundRepeat: 'no-repeat'}} >
+                    <div className="mainImage_content">
+                        <div className="left-65"></div>
                         <div>
-                            <h2>Call of Duty: WWII</h2>
-                            <p>Released on: 23/11/2017</p>
-                            <div class="mainImage_buttons">
-                                <button class="button_green">Order Now</button>
-                                <button class="button_green">More Games</button>
+                            <h2>Call of Duty: WII</h2>
+                            <p>Released on: 20/11/2017 </p>
+                            <p></p>
+                            <div classNameName="mainImage_buttons">
+                                <button className="button_green rmargin-15">Order Now</button>
+                                <button className="button_gray">More Games</button>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="trailer">
-                    <h2 class="trailer_title">Call of Duty: WWII</h2>
-                    <div class="trailer_buttons">
-                        <button class="button_green">Order Now</button>
-                        <button class="button_gray">Technical Specifications</button>
+                <div className="trailer">
+                    <h2 className="trailer_title">Call of Duty: WWII</h2>
+                    <div className="trailer_buttons">
+                        <button className="button_green rmargin-15">Order Now</button>
+                        <button className="button_gray">Technical Specifications</button>
                     </div>
-                    <div class="trailer_video">
-                        <video controls="controls">
-                            <source src={WarVideo} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-                        </video>
-                    </div>
-                </div>
-
-                <div class="stills">
-                    <div class="stills_line">
-                        <p class="stills_text">Life Advice Looking Through A Window</p>
-                        <img src={War1} alt="stillsOne" class="left-200" />
-                    </div>
-                    <div class="stills_line">
-                        <img src={War2} alt="stillsTwo" class="right-200" />
-                        <p class="stills_text">The Best Answers</p>
+                    <div className="trailer_video">
+                        <ReactPlayer
+                            url="https://youtu.be/nKuQwlbqTGk"
+                        />
                     </div>
                 </div>
 
-                <div class="specifications">
+                <div className="stills">
+                    <div className="stills_line">
+                        <p className="stills_text">Life Advice Looking Through A Window</p>
+                        <img src={War1} alt="stillsOne" className="left-200" />
+                    </div>
+                    <div className="stills_line">
+                        <img src={War2} alt="stillsTwo" className="right-200" />
+                        <p className="stills_text">The Best Answers</p>
+                    </div>
+                </div>
+
+                <div className="specifications">
                     <h2>Technical Specifications</h2>
-                    <div class="specifications_wrap">
-                        <p class="specifications_text">
+                    <div className="specifications_wrap">
+                        <p className="specifications_text">
                             Computer Hardware Desktops And Notebooks And Handhelds Oh My
                         </p>
-                        <p class="specifications_text">
+                        <p className="specifications_text">
                             For Women Only Your Computer Usage Could Cost You Your Job
                         </p>
-                        <p class="specifications_text">
+                        <p className="specifications_text">
                             Party Jokes Startling But Unnecessary
                         </p>
-                        <p class="specifications_text">
+                        <p className="specifications_text">
                             Microsoft Patch Management For Home Users
                         </p>
-                        <p class="specifications_text">
+                        <p className="specifications_text">
                             Going Wireless With Your Headphones
                         </p>
-                        <p class="specifications_text">
+                        <p className="specifications_text">
                             Get The Boot A Birds Eye Look Into Mcse Boot Camps
                         </p>
                     </div>
-                    <div class="specifications_buttons">
-                        <button class="button_green">XBOX Consoles</button>
-                        <button class="button_gray">Start streaming on mixer</button>
+                    <div className="specifications_buttons">
+                        <button className="button_green rmargin-15">XBOX Consoles</button>
+                        <button className="button_gray">Start streaming on mixer</button>
                     </div>
                 </div>
             </div>  
--- src/Pages/Games.js
+++ src/Pages/Games.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import data from '../bdgames.json';
+import {useNavigate} from "react-router-dom";
 
-export default function Games () {
-    let games = data.games;
+export default function Games (props) {
+    let navigate = useNavigate();
+    const goToDetails = () => {
+        navigate("/details");
+    }
+
     return (
         <>
         <div className="page-container">
-                <h2 className="page-title">Consoles Games</h2>
+                <h2 className="page-title">{props.title}</h2>
                 <div className="page-container">
 
                 <div className="card-container">
                     {
-                        Object.keys(games).map((el) => {
+                        Object.keys(props.games).map((el) => {
                             return (
-                                <div className="card">
+                                <div className="card dark-fon">
                                     <div className="card__img">
-                                        <img src={games[el].cover} alt={games[el].name}/>
+                                        <img src={props.games[el].cover} alt={props.games[el].name}/>
                                     </div>
                                     <div className="card__content">
-                                        <h3>{games[el].name}</h3>
-                                        <h4>{games[el].subtitle}</h4>
+                                        <h3>{props.games[el].name}</h3>
+                                        <h4>{props.games[el].subtitle}</h4>
+                                        <p className="white-text">{props.games[el].description}</p>
                                     </div>
-                                        <button className="button_green">Show details</button>
+                                        <button className="button_green" onClick={goToDetails}>Show details</button>
                                 </div>
                             )
                         })
